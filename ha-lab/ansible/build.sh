7d34f533f4f63c7087c42469b85c8b15824bb4c0
dwhite <Dan White dwhite@cafedemocracy.org>
2018-02-11 21:34:12 -0600

    Fabric is working!

--- ha-lab/ansible/build.sh
+++ ha-lab/ansible/build.sh
@@ -2,3 +2,9 @@
 
 #ansible-playbook --connection=local -i hosts clonable-containers.yml
 ansible-playbook --connection=local -i hosts site-containers.yml
+
+for foo in host_vars/*
+do
+  sudo lxc-stop -n `basename $foo`
+  sudo lxc-start -n `basename $foo`
+done
