f1bd7d7ed96f1ff0b165421e22cd197c38c82f37
dwhite <Dan White dwhite@cafedemocracy.org>
2018-02-25 19:58:36 -0600

    Initial dns configuration.

--- ha-lab/ansible/build.sh
+++ ha-lab/ansible/build.sh
@@ -17,19 +17,7 @@ network 0.0.0.0/0'
 sudo ifconfig bruplink_24 192.168.100.1/30
 sudo route add -net 192.0.2.0/24 gw 192.168.100.2
 
-for foo in host_vars/*
-do
-  sudo lxc-attach -n `basename $foo` -- bash -l -s << EOF
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "root:passw0rd" | chpasswd
-apt-get -y install openssh-server
-sed -i "s/^#PermitRoot.*/PermitRootLogin yes/" /etc/ssh/sshd_config
-service ssh reload
-EOF
-done
-
-#ansible-playbook -i hosts -e "ansible_user=root ansible_ssh_pass=passw0rd ansible_sudo_pass:passw0rd" site.yml
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook -i hosts -e "ansible_user=root ansible_ssh_pass=passw0rd host_key_checking=False" site.yml
+#ansible-playbook -i hosts -e "ansible_user=root ansible_ssh_pass=passw0rd host_key_checking=False" site.yml
 
 exit 0
--- ha-lab/ansible/lxc-templates/preseed/configscript.sh
+++ ha-lab/ansible/lxc-templates/preseed/configscript.sh
@@ -1,22 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
-
-echo '!!!Beging script!!!'
 export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
 export LC_ALL=C LANGUAGE=C LANG=C
 #/var/lib/dpkg/info/slapd.preinst install
-echo '!!!Starting seed loop!!!'
 if [ -d /tmp/preseeds/ ]; then
         for file in `ls -1 /tmp/preseeds/*`; do
         debconf-set-selections $file
         done
-#echo -n "adminpw = "
-#db_get slapd/internal/adminpw
-#echo "$RET"
 fi
-echo '!!!Finished seed loop!!!'
-dpkg --configure -a
 mount proc -t proc /proc
+mknod ./dev/random c 1 8
+chmod 640 ./dev/random
+chown 0:0 ./dev/random
+mknod ./dev/urandom c 1 9
+chmod 640 ./dev/urandom
+chown 0:0 ./dev/urandom
+/etc/init.d/nslcd stop
+update-rc.d nslcd disable
 dpkg --configure -a
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "root:passw0rd" | chpasswd
+sed -i "s/^#PermitRoot.*/PermitRootLogin yes/" /etc/ssh/sshd_config
+service ssh reload
 umount proc
