e9c01ddb1dc8f3dd774bf006d0599035afc60592
dwhite <Dan White dwhite@cafedemocracy.org>
2020-03-02 12:06:37 -0600

    Add a couple of go-server/grpc examples

---
+++ go/grpc-docker-chat/go-server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "context"
+    "net"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/reflection"
+    "../sdk/go"
+)
+
+type server struct{}
+
+func main() {
+    listener, err := net.Listen("tcp", ":4040")
+    if err != nil {
+        panic(err)
+    }
+
+    srv := grpc.NewServer()
+    proto.RegisterMathServiceServer(srv, &server{})
+    reflection.Register(srv)
+
+    if e := srv.Serve(listener); e != nil {
+        panic(err)
+    }
+}
+
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+    a, b := request.GetA(), request.GetB()
+
+    result := a + b
+
+    return &proto.Response{Result: result}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+    a, b := request.GetA(), request.GetB()
+
+    result := a * b
+
+    return &proto.Response{Result: result}, nil
+}
---
+++ go/grpc-docker-chat/sdk/dart/service.pbjson.dart
@@ -0,0 +1,35 @@
+///
+//  Generated code. Do not modify.
+//  source: service.proto
+//
+// @dart = 2.3
+// ignore_for_file: camel_case_types,unnecessary_const,non_constant_identifier_names,library_prefixes,unused_import,unused_shown_name,return_of_invalid_type
+
+const Request$json = const {
+  '1': 'Request',
+  '2': const [
+    const {'1': 'a', '3': 1, '4': 1, '5': 3, '10': 'a'},
+    const {'1': 'b', '3': 2, '4': 1, '5': 3, '10': 'b'},
+  ],
+};
+
+const Response$json = const {
+  '1': 'Response',
+  '2': const [
+    const {'1': 'result', '3': 1, '4': 1, '5': 3, '10': 'result'},
+  ],
+};
+
+const MathServiceBase$json = const {
+  '1': 'MathService',
+  '2': const [
+    const {'1': 'Add', '2': '.proto.Request', '3': '.proto.Response'},
+    const {'1': 'Multiply', '2': '.proto.Request', '3': '.proto.Response'},
+  ],
+};
+
+const MathServiceBase$messageJson = const {
+  '.proto.Request': Request$json,
+  '.proto.Response': Response$json,
+};
+
