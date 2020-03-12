7fa4cd87667e4fe66a8be8e879c7750e410553d1
Dan White <whitehse@gmail.com>
2020-03-12 16:39:56 +0000

    Add openssl script to list various features and algorithms

---
+++ certificate-authority/openssl-list.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#standard-commands | digest-commands | cipher-commands | cipher-algorithms | digest-algorithms |
+#       public-key-algorithms
+
+# -help                   Display this summary
+# -1                      List in one column
+# -commands               List of standard commands
+# -digest-commands        List of message digest commands
+# -digest-algorithms      List of message digest algorithms
+# -cipher-commands        List of cipher commands
+# -cipher-algorithms      List of cipher algorithms
+# -public-key-algorithms  List of public key algorithms
+# -public-key-methods     List of public key methods
+# -disabled               List of disabled features
+# -missing-help           List missing detailed help strings
+# -options val            List options for specified command
+
+echo "================="
+echo "standard-commands"
+echo "================="
+openssl list -commands
+echo ""
+
+echo "================="
+echo "digest-commands"
+echo "================="
+openssl list -digest-commands
+echo ""
+
+echo "================="
+echo "cipher-commands"
+echo "================="
+openssl list -cipher-commands
+echo ""
+
+echo "================="
+echo "cipher-algorithms"
+echo "================="
+openssl list -cipher-algorithms
+echo ""
+
+echo "================="
+echo "digest-algorithms"
+echo "================="
+openssl list -digest-algorithms
+echo ""
+
+echo "====================="
+echo "public-key-algorithms"
+echo "====================="
+openssl list -public-key-algorithms
+echo ""
+
+echo "=================="
+echo "public-key-methods"
+echo "=================="
+openssl list -public-key-methods
+echo ""
+
+echo "================="
+echo "disabled-features"
+echo "================="
+openssl list -disabled
+echo ""
+
