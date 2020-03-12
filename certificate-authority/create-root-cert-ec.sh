17e4dea50d8bbf9392e43bf0e3391a86fc1f1ae5
Dan White <whitehse@gmail.com>
2020-03-12 12:02:25 +0000

    Create experimental certificate authority using EC keys

---
+++ certificate-authority/create-root-cert-ec.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+## Experimental
+#
+#TEMPDIR=`mktemp -d`
+#
+## Create an EC parameters file
+#openssl ecparam -name prime256v1 -out "$TEMPDIR"/prime256v1.pem
+#
+## Display the data in the parameters file
+#openssl ecparam -in "$TEMPDIR"/prime256v1.pem -text -noout
+##ASN1 OID: prime256v1
+##NIST CURVE: P-256
+#
+## And with
+##echo ""
+##openssl ecparam -in "$TEMPDIR"/prime256v1.pem -text -param_enc explicit -noout
+#
+## Generate key
+#openssl ecparam -in "$TEMPDIR"/prime256v1.pem -genkey -noout -out "$TEMPDIR"/ca.key.pem
+#
+#rm "$TEMPDIR"/*
+#rmdir "$TEMPDIR"
+#
+#exit
+
+if [ -z "$2" ]; then
+  cat - << EOF
+Must provide two options:
+ ./create-root-cert.sh <output_directory> <subject>
+
+For example:
+ ./create-root-cert.sh ca/ "/C=US/ST=OK/L=Bixby/O=Example Ltd./CN=Root CA"
+EOF
+  exit 1
+fi
+
+export DIR="$1"
+export SUBJECT="$2"
+
+if [ ! -d "$DIR" ]; then
+  echo "$DIR must exist"
+  exit 2
+fi
+
+if [ -d "$DIR/certs" ]; then
+  echo "$DIR/certs exists, indicating an existing root CA. Exiting"
+  exit 3
+fi
+
+mkdir -p "$DIR"/{certs,crl,newcerts,private}
+
+CUR_DIR=`pwd`
+cd "$DIR"
+
+chmod 700 private
+touch ./index.txt
+#echo 1000 > serial
+#openssl genrsa -out private/ca.key.pem 4096
+openssl ecparam -name prime256v1 -out ./prime256v1.pem
+openssl ecparam -in ./prime256v1.pem -genkey -noout -out private/ca.key.pem
+chmod 400 private/ca.key.pem
+openssl req -config "$CUR_DIR"/root-config.txt \
+      -key private/ca.key.pem \
+      -new -x509 -days 7300 -sha256 -extensions v3_ca \
+      -out certs/ca.cert.pem \
+	  -subj "$SUBJECT"
+chmod 444 certs/ca.cert.pem
