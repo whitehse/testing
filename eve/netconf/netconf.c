9e299fd96661bf01eb36273a9709429818bf4073
Dan White <whitehse@gmail.com>
2024-04-30 22:07:05 -0500

    Misc

--- eve/netconf/netconf.c
+++ eve/netconf/netconf.c
@@ -168,16 +168,12 @@ int main(int argc, char **argv) {
   if (user == NULL)
     ERROR("Unspecified user name\n");
 
-  //int number_of_e7s = 5;
-  //char *e7s[] = {"192.168.35.11", "192.168.35.12", "192.168.35.13", "192.168.35.14", "192.168.35.15"};
+  int number_of_e7s = 5;
+  char *e7s[] = {"192.168.35.11", "192.168.35.12", "192.168.35.13", "192.168.35.14", "192.168.35.15"};
 
-  int number_of_e7s = 1;
-  char *e7s[] = {"192.168.35.15"};
+  //int number_of_e7s = 1;
+  //char *e7s[] = {"192.168.35.15"};
 
-  const char *hostname = argv[1];
-  //printf("hostname = %s\n", hostname);
-  //printf("e7s[0] = %s\n", e7s[0]);
-  //const char *hostname2 = "192.168.35.14";
   const char *command = argv[2];
   const char *port = "22";
   //const char *port = "830";
@@ -189,204 +185,82 @@ int main(int argc, char **argv) {
     .ai_socktype = SOCK_STREAM,
   };
 
-  //ssh_t ssh[number_of_e7s];
-  ssh_t ssh;
-  //ssh_t ssh2;
-  //int sock[number_of_e7s];
-  int sock;
-  //struct addrinfo *servinfo[number_of_e7s];
-  //struct addrinfo *si[number_of_e7s];
-  struct addrinfo *servinfo;
-  struct addrinfo *si;
+  ssh_t ssh[number_of_e7s];
+  int sock[number_of_e7s];
+  struct addrinfo *servinfo[number_of_e7s];
+  struct addrinfo *si[number_of_e7s];
 
   signal(SIGPIPE, SIG_IGN);
 
   printf("Starting loop\n");
   for (int i=0; i<number_of_e7s; i++) {
     printf("Setting up socket\n");
-    //sock[i] = -1;
-    sock = -1;
-    //if (!getaddrinfo(e7s[i], port, &hints, &(servinfo[i]))) {
-    if (!getaddrinfo(e7s[i], port, &hints, &servinfo)) {
-      //for (si[i] = servinfo[i]; si[i] != NULL; si[i] = si[i]->ai_next) {
-      for (si = servinfo; si != NULL; si = si->ai_next) {
-        //sock[i] = socket(si[i]->ai_family, si[i]->ai_socktype, si[i]->ai_protocol);
-        //sock[i] = socket(si->ai_family, si->ai_socktype, si->ai_protocol);
-        sock = socket(si->ai_family, si->ai_socktype, si->ai_protocol);
-        //if (sock[i] < 0)
-        if (sock < 0)
+    sock[i] = -1;
+    if (!getaddrinfo(e7s[i], port, &hints, &(servinfo[i]))) {
+      for (si[i] = servinfo[i]; si[i] != NULL; si[i] = si[i]->ai_next) {
+        sock[i] = socket(si[i]->ai_family, si[i]->ai_socktype, si[i]->ai_protocol);
+        if (sock[i] < 0)
           continue;
 
-        //if (connect(sock[i], si[i]->ai_addr, si[i]->ai_addrlen)) {
-        //if (connect(sock[i], si->ai_addr, si->ai_addrlen)) {
-        if (connect(sock, si->ai_addr, si->ai_addrlen)) {
-          //close(sock[i]);
-          close(sock);
-          //sock[i] = -1;
-          sock = -1;
+        if (connect(sock[i], si[i]->ai_addr, si[i]->ai_addrlen)) {
+          close(sock[i]);
+          sock[i] = -1;
           continue;
         }
 
         break;
       }
-      freeaddrinfo(servinfo);
+      freeaddrinfo(servinfo[i]);
     }
-    //if (sock[i] < 0)
-    if (sock < 0)
+    if (sock[i] < 0)
       ERROR("Unable to connect: %s\n", strerror(errno));
 
     printf("Socket was opened for %s\n", e7s[i]);
-/*
-  int sock = -1;
-  struct addrinfo *servinfo, *si;
-  if (!getaddrinfo(hostname, port, &hints, &servinfo)) {
-    for (si = servinfo; si != NULL; si = si->ai_next) {
-      sock = socket(si->ai_family, si->ai_socktype, si->ai_protocol);
-      if (sock < 0)
-        continue;
 
-      if (connect(sock, si->ai_addr, si->ai_addrlen)) {
-        close(sock);
-        sock = -1;
-        continue;
-      }
+  struct assh_context_s *context[number_of_e7s];
+  context[i] = malloc(sizeof(struct assh_context_s));
 
-      break;
-    }
-
-    freeaddrinfo(servinfo);
-  }
-  if (sock < 0)
-    ERROR("Unable to connect: %s\n", strerror(errno));
-
-  int sock2 = -1;
-  struct addrinfo *servinfo2, *si2;
-  if (!getaddrinfo(hostname2, port, &hints, &servinfo2)) {
-    for (si2 = servinfo2; si2 != NULL; si2 = si2->ai_next) {
-      sock2 = socket(si2->ai_family, si2->ai_socktype, si2->ai_protocol);
-      if (sock2 < 0)
-        continue;
-
-      if (connect(sock2, si2->ai_addr, si2->ai_addrlen)) {
-        close(sock2);
-        sock2 = -1;
-        continue;
-      }
-
-      break;
-   }
-
-   freeaddrinfo(servinfo2);
-  }
-
-  if (sock2 < 0)
-    ERROR("Unable to connect: %s\n", strerror(errno));
-*/
-
-  //struct assh_context_s *context[number_of_e7s];
-  //struct assh_context_s *context2;
-  struct assh_context_s *context;
-
-    if (assh_context_create(&context, ASSH_CLIENT,
+    if (assh_context_create(&context[i], ASSH_CLIENT,
                             NULL, NULL, NULL, NULL) ||
-        assh_service_register_default(context) ||
-        assh_algo_register_default(context, ASSH_SAFETY_WEAK))
+        assh_service_register_default(context[i]) ||
+        assh_algo_register_default(context[i], ASSH_SAFETY_WEAK))
       ERROR("Unable to create an assh context.\n");
 
     printf("Finished assh_context_create for %s\n", e7s[i]);
-/*
-  if (assh_context_create(&context, ASSH_CLIENT,
-                          NULL, NULL, NULL, NULL) ||
-      assh_service_register_default(context) ||
-      assh_algo_register_default(context, ASSH_SAFETY_WEAK))
-    ERROR("Unable to create an assh context.\n");
 
-  if (assh_context_create(&context2, ASSH_CLIENT,
-                          NULL, NULL, NULL, NULL) ||
-      assh_service_register_default(context2) ||
-      assh_algo_register_default(context2, ASSH_SAFETY_WEAK))
-    ERROR("Unable to create an assh context2.\n");
-*/
+    struct assh_session_s *session[number_of_e7s];
+    session[i] = malloc(sizeof(struct assh_session_s));
 
-  //struct assh_session_s *session;
-  //struct assh_session_s *session2;
-  //struct assh_session_s *session[number_of_e7s];
-  struct assh_session_s *session;
-
-    //if (assh_session_create(context[i], &(session[i])))
-    //if (assh_session_create(context[i], &session))
-    if (assh_session_create(context, &session))
+    if (assh_session_create(context[i], &session[i]))
       ERROR("Unable to create an assh session.\n");
 
     printf("Finished assh_session_create for %s\n", e7s[i]);
-
-  //if (assh_session_create(context2, &session2))
-  //  ERROR("Unable to create an assh session2.\n");
 
     enum assh_userauth_methods_e auth_methods =
       ASSH_USERAUTH_METHOD_PASSWORD;/* |
     ASSH_USERAUTH_METHOD_PUBKEY |
     ASSH_USERAUTH_METHOD_KEYBOARD;*/
 
-//  enum assh_userauth_methods_e auth_methods2 =
-//    ASSH_USERAUTH_METHOD_PASSWORD;/* |
-//    ASSH_USERAUTH_METHOD_PUBKEY |
-//    ASSH_USERAUTH_METHOD_KEYBOARD;*/
-
-    //struct asshh_client_inter_session_s inter[number_of_e7s];
-    struct asshh_client_inter_session_s inter;
-    //asshh_client_init_inter_session(&inter[i], command, NULL);
-    asshh_client_init_inter_session(&inter, command, NULL);
+    struct asshh_client_inter_session_s *inter[number_of_e7s];
+    inter[i] = malloc(sizeof(struct asshh_client_inter_session_s));
+    asshh_client_init_inter_session(inter[i], command, NULL);
 
     printf("Finished client_init_inter_session for %s\n", e7s[i]);
 
-    //struct asshh_client_inter_session_s inter2;
-    //asshh_client_init_inter_session(&inter2, command, NULL);
-    //asshh_client_init_inter_session(&inter2, NULL, NULL);
-
-  //struct asshh_inter_subsystem_s sub;
-  //asshh_inter_init_subsystem(&sub, "netconf");
-
-    //struct assh_event_s event[number_of_e7s];
-    //struct assh_event_s event2;
-    //struct assh_event_s event;
-
-    //ssh[i].session = session[i];
-    //ssh[i].inter = &inter[i];
-    //ssh[i].hostname = e7s[i];
-    //ssh[i].user = user;
-    //ssh[i].auth_methods = &auth_methods;
-    ssh.session = session;
-    ssh.inter = &inter;
-    ssh.hostname = e7s[i];
-    ssh.user = user;
-    ssh.auth_methods = &auth_methods;
+    ssh[i].session = session[i];
+    ssh[i].inter = inter[i];
+    ssh[i].hostname = e7s[i];
+    ssh[i].user = user;
+    ssh[i].auth_methods = &auth_methods;
 
     printf("Finished setting ssh for %s\n", e7s[i]);
-  //ssh2.session = session2;
-  //ssh2.inter = &inter2;
-  //ssh2.hostname = hostname2;
-  //ssh2.user = user;
-  //ssh2.auth_methods = &auth_methods2;
 
-    //ev_io *socket_watcher[number_of_e7s];
-    //socket_watcher[i] = malloc(number_of_e7s * sizeof(ev_io));
-    //ev_io socket_watcher[number_of_e7s];
-    ev_io socket_watcher;
+    ev_io *socket_watcher[number_of_e7s];
+    socket_watcher[i] = malloc(sizeof(ev_io));
 
-    //ev_io_init (socket_watcher[i], socket_cb, sock[i], EV_READ|EV_WRITE);
-    //ev_io_init (&(socket_watcher[i]), socket_cb, sock[i], EV_READ|EV_WRITE);
-    //ev_io_init (&socket_watcher, socket_cb, sock[i], EV_READ|EV_WRITE);
-    ev_io_init (&socket_watcher, socket_cb, sock, EV_READ|EV_WRITE);
-
-    //socket_watcher[i]->data = &ssh[i];
-    //(&(socket_watcher[i]))->data = &(ssh[i]);
-    socket_watcher.data = &ssh;
-
-    //ev_io_start (loop, socket_watcher[i]);
-    //ev_io_start (loop, &(socket_watcher[i]));
-    ev_io_start (loop, &socket_watcher);
-
+    ev_io_init (socket_watcher[i], socket_cb, sock[i], EV_READ|EV_WRITE);
+    socket_watcher[i]->data = &ssh[i];
+    ev_io_start (loop, socket_watcher[i]);
   }
 
   ev_timer_init (&timeout_watcher, timeout_cb, 0.5, 4.);
