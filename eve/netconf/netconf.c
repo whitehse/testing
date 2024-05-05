c64724eec4ac779eeff94a1371f067c6600be053
Dan White <whitehse@gmail.com>
2024-05-04 20:29:23 -0500

    Add basic libwebsockets support into libev loop

--- eve/netconf/netconf.c
+++ eve/netconf/netconf.c
@@ -31,6 +31,7 @@
 #include <assh/assh_algo.h>
 
 #include <ev.h>
+#include <libwebsockets.h>
 #include <netconf.h>
 
 #define ERROR(...) do { fprintf(stderr, __VA_ARGS__); exit(1); } while (0)
@@ -41,9 +42,16 @@ ev_timer timeout_watcher;
 time_t seconds;
 
 #define BUFF_SZ   512
-
 char buff[BUFF_SZ + 1];
 struct io_uring ring;
+
+static const struct lws_http_mount mount = {
+        .mountpoint     = "/",        /* mountpoint URL */
+        .origin       = "./tmp/mount-origin", /* serve from dir */
+        .def        = "index.html",   /* default filename */
+        .origin_protocol    = LWSMPRO_FILE,     /* files in a dir */
+        .mountpoint_len     = 1,            /* char count */
+};
 
 int setup_io_uring(int efd) {
   int ret = io_uring_queue_init(8, &ring, 0);
@@ -422,11 +430,54 @@ int main(int argc, char **argv) {
   io_uring_prep_read(sqe, fd, buff, BUFF_SZ, 0);
   io_uring_submit(&ring);
 
+  struct lws_client_connect_info connect_info;
+  void *foreign_loops[1] = { loop };
+  int BUF_SIZE = 4096;
+
+  struct lws_context_creation_info info;
+  struct lws_context *context;
+  const char *p;
+  int n = 0, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
+    /* for LLL_ verbosity above NOTICE to be built into lws,
+     * lws must have been configured and built with
+     * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
+    /* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
+    /* | LLL_EXT */ /* | LLL_CLIENT */ /* | LLL_LATENCY */
+    /* | LLL_DEBUG */;
+
+  //signal(SIGINT, sigint_handler);
+
+  //if ((p = lws_cmdline_option(argc, argv, "-d")))
+  //     logs = atoi(p);
+
+  lws_set_log_level(logs, NULL);
+  lwsl_user("LWS minimal http server | visit http://localhost:7681\n");
+
+  memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
+  info.port = 7681;
+  info.mounts = &mount;
+  info.error_document_404 = "/404.html";
+  info.options =
+    LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE | LWS_SERVER_OPTION_LIBEV;
+  info.foreign_loops = foreign_loops;
+  //info.uid = 1000;
+  //info.gid = 1000;
+
+  //if (lws_cmdline_option(argc, argv, "--h2-prior-knowledge"))
+  //    info.options |= LWS_SERVER_OPTION_H2_PRIOR_KNOWLEDGE;
+
+  context = lws_create_context(&info);
+  if (!context) {
+    lwsl_err("lws init failed\n");
+    return 1;
+  }
+
   ev_run (loop, 0);
 
   abort();
 
   printf("Connection closed\n");
 
+  lws_context_destroy(context);
   //assh_session_release(session);
 }
