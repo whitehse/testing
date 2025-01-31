911adb3be88d49042302af429524b6c36a76af8f
Dan White <whitehse@gmail.com>
2025-01-30 19:14:48 -0600

    Misc

--- ipfix/process_ipfix_iana.pl
+++ ipfix/process_ipfix_iana.pl
@@ -4,6 +4,12 @@ use Text::CSV qw( csv );
 use Data::Dumper;
 use strict;
 
+print <<'EOF';
+#ifndef IPFIX_IANA_H
+#define IPFIX_IANA_H
+
+EOF
+
 my $icmp_hash = csv (in => "icmp-parameters-types.csv",
                      headers => "auto");   # as array of hash
 
@@ -16,35 +22,28 @@ my $proto_hash = csv (in => "protocol-numbers-1.csv",
 my $ipfix_hash = csv (in => "ipfix-information-elements.csv",
                       headers => "auto");   # as array of hash
 
-print <<'EOF';
-#ifndef IPFIX_IANA_H
-#define IPFIX_IANA_H
-
-enum IANA_ABSTRACT_TYPE {
-  IANA_BASICLIST,
-  IANA_BOOLEAN,
-  IANA_DATETIMEMICROSECONDS,
-  IANA_DATETIMEMILLISECONDS,
-  IANA_DATETIMENANOSECONDS,
-  IANA_DATETIMESECONDS,
-  IANA_FLOAT64,
-  IANA_IPV4ADDRESS,
-  IANA_IPV6ADDRESS,
-  IANA_MACADDRESS,
-  IANA_OCTETARRAY,
-  IANA_SIGNED32,
-  IANA_STRING,
-  IANA_SUBTEMPLATELIST,
-  IANA_SUBTEMPLATEMULTILIST,
-  IANA_UNSIGNED8,
-  IANA_UNSIGNED16,
-  IANA_UNSIGNED32,
-  IANA_UNSIGNED64,
-  IANA_UNSIGNED256
-};
-
-
-EOF
+#enum IANA_ABSTRACT_TYPE {
+#  IANA_BASICLIST,
+#  IANA_BOOLEAN,
+#  IANA_DATETIMEMICROSECONDS,
+#  IANA_DATETIMEMILLISECONDS,
+#  IANA_DATETIMENANOSECONDS,
+#  IANA_DATETIMESECONDS,
+#  IANA_FLOAT64,
+#  IANA_IPV4ADDRESS,
+#  IANA_IPV6ADDRESS,
+#  IANA_MACADDRESS,
+#  IANA_OCTETARRAY,
+#  IANA_SIGNED32,
+#  IANA_STRING,
+#  IANA_SUBTEMPLATELIST,
+#  IANA_SUBTEMPLATEMULTILIST,
+#  IANA_UNSIGNED8,
+#  IANA_UNSIGNED16,
+#  IANA_UNSIGNED32,
+#  IANA_UNSIGNED64,
+#  IANA_UNSIGNED256
+#};
 
 #foreach my $row (@$aoh) {
 #  print %$row{'Abstract Data Type'} . "\n";
