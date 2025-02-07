0bef69d759203fc754c8fa225f45a6bc096c09ea
Dan White <whitehse@gmail.com>
2025-02-07 15:40:35 -0600

    Misc

--- eve/netconf/gdb_remote.sh
+++ eve/netconf/gdb_remote.sh
@@ -2,5 +2,6 @@
 
 # cat >> ~/gdbinit <EOF
 # set sysroot /
+# #set substitute-path /usr/src/include /mnt/include
 # EOF 
 gdbtui -ex="target remote | ssh -T map.ecolink.coop gdbserver - ./netconf" ./netconf
